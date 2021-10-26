8bceac1f6498737991be3908c6d28b17ecda45df
Amit Pharande <amitpharande16@gmail.com>
2021-10-26 19:41:17 +0530

    First Commit

--- SecondCode.java
+++ SecondCode.java
@@ -1,5 +1,6 @@
 class SecondCode {
     public static void main(String args[]){
         System.out.println("Hello Visual Studio Code");
+        System.out.println("Understood Git and Github");
     }
 }
